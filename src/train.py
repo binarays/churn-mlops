7f61cf6b3d145f28f6cd0e87816fb3aca5203f9b
Binara <binarayasas2003@gmail.com>
2026-03-06 23:32:05 +0530

    Update pipeline

--- src/train.py
+++ src/train.py
@@ -15,20 +15,17 @@ from preprocessing import preprocess_data
 # DAGsHub MLflow Configuration
 # -----------------------------
 
-# Get values from environment variables (SECURE WAY)
-USERNAME = os.getenv("DAGSHUB_USERNAME")
-TOKEN = os.getenv("DAGSHUB_TOKEN")
+USERNAME = "binarays" #os.getenv("DAGSHUB_USERNAME")
+TOKEN = "956ba71c582b8b866d8721df9cced449f381f33e" #os.getenv("DAGSHUB_TOKEN")
 REPO = "churn-mlops"
 
 if USERNAME is None or TOKEN is None:
     print("WARNING: DAGsHub credentials not found in environment variables.")
 
-# Set tracking URI
 mlflow.set_tracking_uri(
     f"https://dagshub.com/{USERNAME}/{REPO}.mlflow"
 )
 
-# Set authentication (for DAGsHub)
 os.environ["MLFLOW_TRACKING_USERNAME"] = USERNAME if USERNAME else ""
 os.environ["MLFLOW_TRACKING_PASSWORD"] = TOKEN if TOKEN else ""
 
@@ -53,35 +50,61 @@ def train_models():
         "xgboost": XGBClassifier(eval_metric="logloss")
     }
 
+    best_model = None
+    best_accuracy = 0
+    best_model_name = ""
+
     for name, model in models.items():
 
         with mlflow.start_run(run_name=name):
 
             print(f"Training {name}")
 
-            # Train model
+            # Train
             model.fit(X_train, y_train)
 
-            # Predictions
+            # Predict
             predictions = model.predict(X_test)
 
             # Accuracy
             accuracy = accuracy_score(y_test, predictions)
 
-            # Log metrics
+            # Log to MLflow
             mlflow.log_param("model_name", name)
             mlflow.log_metric("accuracy", accuracy)
 
-            # Save model locally
+            # Save model
             model_path = f"models/{name}.pkl"
             joblib.dump(model, model_path)
 
-            # Log model to MLflow
+            # Log model artifact
             mlflow.sklearn.log_model(model, "model")
 
             print(f"{name} completed with accuracy: {accuracy}")
 
-    print("All models trained successfully.")
+            # Check if best model
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                best_model = model
+                best_model_name = name
+
+    # Save best model
+    best_model_path = "models/best_model.pkl"
+    joblib.dump(best_model, best_model_path)
+
+    print("\nBest Model Selected")
+    print(f"Model: {best_model_name}")
+    print(f"Accuracy: {best_accuracy}")
+
+    # Log best model to MLflow
+    with mlflow.start_run(run_name="best_model"):
+
+        mlflow.log_param("best_model_name", best_model_name)
+        mlflow.log_metric("best_accuracy", best_accuracy)
+
+        mlflow.sklearn.log_model(best_model, "best_model")
+
+    print("Best model saved as models/best_model.pkl")
 
 
 if __name__ == "__main__":
