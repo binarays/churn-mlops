48249afc8063c628649d4fb7d7a6d73d88dcc935
Binara <binarayasas2003@gmail.com>
2026-03-10 01:45:32 +0530

    Fixed UI bug

--- static/js/control.js
+++ static/js/control.js
@@ -1,8 +1,3 @@
 function clearFields() {
-    document.getElementById("tenure").value = "";
-    document.getElementById("monthlyCharges").value = "";
-    document.getElementById("totalCharges").value = "";
-    document.getElementById("contract").selectedIndex = 0;
-
-    document.getElementById("result").innerHTML = "";
+    window.location.reload();
 }
